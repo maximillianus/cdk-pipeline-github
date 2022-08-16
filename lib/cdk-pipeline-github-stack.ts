edcbd7f387608c285ee9a537f793fd248bf43702
maximillianus <pradana.aditya@gmail.com>
2022-08-17 04:53:06 +0700

    initial commit

---
+++ lib/cdk-pipeline-github-stack.ts
@@ -0,0 +1,17 @@
+import { Construct } from 'constructs';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import * as cdk from 'aws-cdk-lib';
+
+export class CdkPipelineGithubStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const pipeline = new CodePipeline(this, 'Pipeline', {
+      pipelineName: 'CDK-Pipeline-Github',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('maximillianus/cdk-pipeline-github', 'main'),
+        commands: ['npm ci', 'npm run build', 'npx cdk synth']
+      })
+    });
+  }
+}
